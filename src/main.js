b8a98ee2e755d9e6f7b15421121af61a62486ce9
Harysson <haryssonsoares@gmail.com>
2023-08-31 20:49:02 -0300

    Alterando style com Javascript

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-// CAPTURANDO OS BOTÕES DE CONTROLE
 const controle1 = document.getElementById("controle-1");
 const controle2 = document.getElementById("controle-2");
 const controle3 = document.getElementById("controle-3");
 const controle4 = document.getElementById("controle-4");
 
-// Estrutura de uma funtion:
-/* 
-function <nome_da_funtion>(parametros) {
-  ...escopo
-}
- */
 function selecionarBotao(evento) {
-  console.log(evento);
+  // Primeiro deixo todos com background transparent
+  controle1.style.backgroundColor = "transparent";
+  controle2.style.backgroundColor = "transparent";
+  controle3.style.backgroundColor = "transparent";
+  controle4.style.backgroundColor = "transparent";
+
+  // Depois eu pego o algo (target) do click e pinto ele de branco
+  evento.target.style.backgroundColor = "white";
 }
