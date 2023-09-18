31f2fc2e67e99e3e3dc2f5d9f421de6c536a32f3
Aylle666 <aylleryan69@gmail.com>
2023-09-18 16:17:33 -0300

    Atividade: Imprimir elementos da DOM no JS

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// Crie os ids dos contrroles do carrosel
+
+// crie uma função para imprimir no console log os controles
+
+/* 
+  function imprimir o elemento(evento) {
+    console.log(evento.target)
+  }
+
+*/
