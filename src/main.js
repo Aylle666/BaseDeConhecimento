1829ac70292bc29a5412333e26a0fa185032be9a
Aylle666 <aylleryan69@gmail.com>
2023-09-20 15:18:32 -0300

    carrocel semi pronto

--- src/main.js
+++ src/main.js
@@ -12,44 +12,41 @@
 const controle1 = document.getElementById("controle-1");
 const controle2 = document.getElementById("controle-2");
 const controle3 = document.getElementById("controle-3");
-const controle4 = document.getElementById("controle-4");
 
-const DivImagens = document.getElementsByClassName("imagens")[0];
+const divImagens = document.getElementsByClassName("imagens")[0];
 
 function selecionarBotao(evento, espacoEntreCadaImagem) {
   controle1.style.backgroundColor = "transparent";
   controle2.style.backgroundColor = "transparent";
   controle3.style.backgroundColor = "transparent";
-  controle4.style.backgroundColor = "transparent";
 
-  evento.targe.style.backgroundColor = "white";
+  evento.target.style.backgroundColor = "white";
 
   moverImagem(evento.target.id, espacoEntreCadaImagem);
-
-  console.log(DivImagens.offsetwidth);
 }
 
 function moverImagem(controle, espacoEntreCadaImagem) {
-  switch ((controle, espacoEntreCadaImagem)) {
+  switch (controle) {
     case "controle-1":
       document.getElementsByClassName("imagens")[0].style.transform =
         "translateX(0)";
       break;
 
     case "controle-2":
-      DivImagens.style.transform = `translateX(-${DivImagens.offsetwidth}px) trnaslateX(-${espacoEntreCadaImagem}px)`;
+      console.log(espacoEntreCadaImagem);
+      divImagens.style.transform = `translateX(-${divImagens.offsetWidth}px) `;
       break;
 
     case "controle-3":
-      DivImagens.style.transform = `translateX(-${
-        DivImagens.offsetwidth * 2
-      }px) trnaslateX(-${espacoEntreCadaImagem}px)`;
+      divImagens.style.transform = `translateX(-${
+        divImagens.offsetWidth * 2
+      }px) translateX(-${espacoEntreCadaImagem}px)`;
       break;
 
-    case "controle-4":
-      DivImagens.style.transform = `translateX(-${
-        DivImagens.offsetwidth * 3
-      }px) trnaslateX(-${espacoEntreCadaImagem}px)`;
-      break;
+    // case "controle-4":
+    // DivImagens.style.transform = `translateX(-${
+    // DivImagens.offsetwidth * 3
+    // }px) trnaslateX(-${espacoEntreCadaImagem}px)`;
+    // break;
   }
 }
